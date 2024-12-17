8636a8fdbdc8c90aa2bd63804fc38b2b229993bd
Manan <srivastavamanan23@gmail.com>
2024-12-17 14:58:11 +0530

    Add search field test case and page object class

---
+++ src/test/java/pageObjects/SearchPgae.java
@@ -0,0 +1,58 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SearchPgae extends BasePage{
+
+	public SearchPgae(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	// Locator
+	@FindBy(xpath = "//input[@placeholder='Search']")
+	WebElement search_box;
+	
+	@FindBy(xpath = "//button[@class='btn btn-default btn-lg']")
+	WebElement search_btn;
+	
+	@FindBy(xpath ="//body[1]/div[2]/div[1]/div[1]/div[3]/div[2]/div[1]/div[2]/div[1]/h4[1]/a[1]")
+	WebElement Pro1;
+	
+	@FindBy(xpath ="//button[@id='button-cart']")
+	WebElement Cart;
+	
+	@FindBy(xpath = "//div[@class='alert alert-success alert-dismissible']")
+	WebElement success_Alert;
+	
+	// Methods 
+	
+	public void search_item() {
+		search_box.sendKeys("Mac");;
+	}
+	
+	public void srch_btn() {
+		search_btn.click();
+	}
+	
+	public void sel_item() {
+		Pro1.click();
+		
+	}
+	
+	public void addCart() {
+		Cart.click();
+		
+	}
+	
+	public boolean successAddCart() {
+		
+		return success_Alert.getText() != null;
+		}
+		
+
+}
--- src/test/java/testCases/TC_Seach_Product.java
+++ src/test/java/testCases/TC_Seach_Product.java
@@ -1,7 +1,56 @@
 package testCases;
 
-public class TC_Seach_Product {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.HomePage;
+import pageObjects.SearchPgae;
+import pageObjects.loginPage;
+import testBase.BaseClass;
+
+public class TC_Seach_Product extends BaseClass{
+	
+	
+	@Test
+	public void searchtest() {
+		try {
+	
+		// Access home page
+		HomePage home =new HomePage(driver);
+		home.MyAccount();
+		home.login();
+		
+		// Access login page 
+		
+		loginPage login= new loginPage(driver);
+		login.setemail(pr.getProperty("email"));
+		login.setpass(pr.getProperty("pass"));
+		
+		login.lg_btn();
+		
+		
+		// access search
+		
+		SearchPgae srch_page = new SearchPgae(driver);
+		srch_page.search_item();
+		srch_page.srch_btn();
+		srch_page.sel_item();
+		srch_page.addCart();
+		srch_page.successAddCart();
+		
+		boolean target = srch_page.successAddCart();
+		
+		System.out.println(target);
+		
+		Assert.assertEquals(target,true, "Success: You have added MacBook to your shopping cart!");
+		}
+		catch(Exception e) {
+			Assert.fail();
+		}
+		
+
+	}
+	
 
 	
-	// later
 }
